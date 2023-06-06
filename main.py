8b330ce9d2878fb5e86162921baf777ab812b750
kev0051 <126007369+kev0051@users.noreply.github.com>
2023-06-06 14:05:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import tools.data as data
 
 print('bruh')
 
-xmin = 673029.1
+xmin = 673059.1
 bound = 50
 ymin = 3678019.6 
 lasdata = 'initData.las'
@@ -19,13 +19,13 @@ count = 0
 # https://github.com/isl-org/Open3D/issues/1095
 
 # Change the range for number of images to save
-for i in (n+7 for n in range (1)): # n+6 starts saving at train_0005, range(1) saves 1 image for that config
+for i in (n+17 for n in range (1)): # n+6 starts saving at train_0005, range(1) saves 1 image for that config
     v = lasTest.get_voxelization_heat(xmin, ymin, bound, 0.75, lasdata) 
     xmin = xmin + (bound/2) # set for half-window configuration for now
     #voxelization.visualize_voxel_grid(v) # comment this out if you don't want to pull up the image for yourself to view
     
     #comment this out if you don't want to save the image
-    voxelization.capture_voxel_grid(v, i, 'training_data') # i is for filename, string is folder to save to (make sure folder exists in directory)
+    voxelization.capture_voxel_grid(v, i, 'images/train') # i is for filename, string is folder to save to (make sure folder exists in directory)
     
     count = count + 1
 
